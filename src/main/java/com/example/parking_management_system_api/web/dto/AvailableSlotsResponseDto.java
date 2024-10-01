842298b16c158f78f86253fc97d952941b36aeaa
Mateus Lacerda <mateuslacerdadesouza120@gmail.com>
2024-10-01 14:43:09 -0300

    feat: Implement custom exceptions and improve parking space management service

--- src/main/java/com/example/parking_management_system_api/web/dto/AvailableSlotsResponseDto.java
+++ src/main/java/com/example/parking_management_system_api/web/dto/AvailableSlotsResponseDto.java
@@ -1,12 +1,11 @@
 package com.example.parking_management_system_api.web.dto;
 
 
-import jakarta.persistence.Access;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 @AllArgsConstructor
 @Getter
-public class AvailableSlotsResponse {
+public class AvailableSlotsResponseDto {
 
     private final String slotType;
     private final int availableCount;
