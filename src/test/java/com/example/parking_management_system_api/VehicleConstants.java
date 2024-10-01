7a573c3a1db1ddaa440c473addd3c2c66a65250c
daysources <142356916+daysources@users.noreply.github.com>
2024-09-30 22:31:02 -0300

    feat: Complete VehicleRepositoryTest

--- src/test/java/com/example/parking_management_system_api/VehicleConstants.java
+++ src/test/java/com/example/parking_management_system_api/VehicleConstants.java
@@ -8,4 +8,15 @@ public class VehicleConstants {
 
     public static final Vehicle VEHICLE = new Vehicle(1L, "testplate1", VehicleCategoryEnum.SEPARATED, VehicleTypeEnum.PASSENGER_CAR
     , false);
+    public static final Vehicle EMPTY_VEHICLE = new Vehicle();
+    public static final Vehicle INVALID_VEHICLE = new Vehicle(2L, null, VehicleCategoryEnum.MONTHLY_PAYER, VehicleTypeEnum.DELIVERY_TRUCK
+    , false);
+    public static final Vehicle VEHICLE2 = new Vehicle( "testplate2", VehicleCategoryEnum.SEPARATED, VehicleTypeEnum.PASSENGER_CAR
+            , false);
+    public static final Vehicle VEHICLE3 = new Vehicle( "testplate3", VehicleCategoryEnum.MONTHLY_PAYER, VehicleTypeEnum.PASSENGER_CAR
+            , true);
+    public static final Vehicle VEHICLE4 = new Vehicle( "testplate4", VehicleCategoryEnum.SEPARATED, VehicleTypeEnum.MOTORCYCLE
+            , false);
+    public static final Vehicle VEHICLE5 = new Vehicle( "testplate5", VehicleCategoryEnum.DELIVERY_TRUCK, VehicleTypeEnum.DELIVERY_TRUCK
+            , false);
 }
--- src/test/java/com/example/parking_management_system_api/VehicleRepositoryTest.java
+++ src/test/java/com/example/parking_management_system_api/VehicleRepositoryTest.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-import static com.example.parking_management_system_api.VehicleConstants.VEHICLE;
+import static com.example.parking_management_system_api.VehicleConstants.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -22,6 +24,7 @@ public class VehicleRepositoryTest {
     private TestEntityManager testEntityManager;
 
     @Test
+    @Transactional
     public void createVehicle_WithValidData_ReturnsVehicle (){
         Vehicle vehicle = vehicleRepository.save(VEHICLE);
         Vehicle sut = testEntityManager.find(Vehicle.class, vehicle.getId());
@@ -30,5 +33,46 @@ public class VehicleRepositoryTest {
         assertThat(sut.getCategory()).isEqualTo(VehicleCategoryEnum.SEPARATED);
         assertThat(sut.getAccessType()).isEqualTo(VehicleTypeEnum.PASSENGER_CAR);
         assertThat(sut.getRegistered()).isFalse();
+    }
+
+    @Test
+    @Transactional
+    public void createVehicle_WithNullOrInvalidData_ThrowsException(){
+        assertThatThrownBy(()-> vehicleRepository.save(EMPTY_VEHICLE));
+        assertThatThrownBy(()-> vehicleRepository.save(INVALID_VEHICLE));
+    }
+
+    @Test
+    @Transactional
+    public void createVehicle_WithExistingPlate_ThrowsException() {
+        Vehicle vehicle = testEntityManager.persistFlushFind(VEHICLE2);
+        testEntityManager.detach(vehicle);
+        vehicle.setId(null);
+        assertThatThrownBy(()-> vehicleRepository.save(vehicle));
+    }
+
+    @Test
+    @Transactional
+    public void deleteVehicle_WithExistingId_ReturnsNoContent(){
+        Vehicle vehicle = testEntityManager.persistFlushFind(VEHICLE3);
+        testEntityManager.detach(vehicle);
+        vehicleRepository.deleteById(vehicle.getId());
+        assertThat(vehicleRepository.findById(1L)).isEmpty();
+    }
+
+    @Test
+    @Transactional
+    public void findVehicle_WithValidId_ReturnsVehicle(){
+        Vehicle vehicle = testEntityManager.persistFlushFind(VEHICLE4);
+        testEntityManager.detach(vehicle);
+        assertThat(vehicleRepository.findById(vehicle.getId())).isPresent();
+    }
+
+    @Test
+    @Transactional
+    public void findVehicle_WithInvalidId_ReturnsNoContent(){
+        Vehicle vehicle = testEntityManager.persistFlushFind(VEHICLE5);
+        testEntityManager.detach(vehicle);
+        assertThat(vehicleRepository.findById(10L)).isEmpty();
     }
 }
